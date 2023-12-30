7645c701641f47934e8451c99c45ef39d50fd440
NardosYosef <nardosyosef123@gmail.com>
2023-12-30 16:43:56 +0300

    my prtfolio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,9 +16,11 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+         { /* eslint-disable react/no-unescaped-entities */}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello,I&rsquo;m{' '}
             </span>
+            {/* eslint-enable react/no-unescaped-entities */}
             <br></br>
             <TypeAnimation
               sequence={[
